02fba648ea1546c9691531346227127c9786ca8c
nursultanit <nursultanzamirovic@gmail.com>
2024-08-26 11:53:35 +0600

    Describe your changes here

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserProfileSerializer
+from .models import UserProfile
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class Home(APIView):
